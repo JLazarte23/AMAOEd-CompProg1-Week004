c83ef7461915c039abacf297a71434c1f69e2f46
Joey Lazarte <38686223+JLazarte23@users.noreply.github.com>
2018-04-27 14:03:02 +0800

    Add files via upload

---
+++ src/Week007_01.cpp
@@ -0,0 +1,64 @@
+/* ****************************************************************
+ * @author: <your name here>
+ * @app name: <your application name here>
+ * @app desc: <describe the purpose of your application here>
+ * @history:
+ *  - <yyyy/mm/dd> | <name>
+ *      -- <description>
+ *      -- <description>
+ *      -- <description>
+ *  - <yyyy/mm/dd> | <name>
+ *      -- <description>
+ *      -- <description>
+ *      -- <description>
+ * ****************************************************************/
+
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include "_pause.h"
+
+using namespace std;
+
+//////////////////////////////////////////////////////////////////
+//                               NOTE
+// This is your program entry point. Your main logic is placed
+// inside this function. You may add your functions before this
+// "main()", or after this "main()" provided you added reference
+// before this "main()" function.
+//////////////////////////////////////////////////////////////////
+
+int main() {
+    // ************************** TO DO **************************
+    // Place your code logic after this comment line
+    // ***********************************************************
+
+    string num = "";
+    int height = 0, i = 1, j;
+
+    cout << "Enter height of triangle: ";
+    getline(std::cin, num);
+    stringstream(num) >> height;
+
+     while(i<=height)                                                                
+     {                                                                        
+        j=1;                                                                    
+        while(j<=i)                                                             
+        {                                                                      
+             cout << "*";                                                       
+            j++;                                                                 
+        }                                                                      
+        cout << endl;                                                       
+        i++;                                                                   
+   }          
+
+    
+    // ********************** DO NOT CHANGE **********************
+    // Print a new line and ask user for any key before exiting
+    // ***********************************************************
+    cout << "" << endl;
+    _pause();
+    return EXIT_SUCCESS;
+}
---
+++ src/Week007_03.cpp
@@ -0,0 +1,63 @@
+/* ****************************************************************
+ * @author: <your name here>
+ * @app name: <your application name here>
+ * @app desc: <describe the purpose of your application here>
+ * @history:
+ *  - <yyyy/mm/dd> | <name>
+ *      -- <description>
+ *      -- <description>
+ *      -- <description>
+ *  - <yyyy/mm/dd> | <name>
+ *      -- <description>
+ *      -- <description>
+ *      -- <description>
+ * ****************************************************************/
+
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include "_pause.h"
+
+using namespace std;
+
+//////////////////////////////////////////////////////////////////
+//                               NOTE
+// This is your program entry point. Your main logic is placed
+// inside this function. You may add your functions before this
+// "main()", or after this "main()" provided you added reference
+// before this "main()" function.
+//////////////////////////////////////////////////////////////////
+
+int main() {
+    // ************************** TO DO **************************
+    // Place your code logic after this comment line
+    // ***********************************************************
+
+    string num = "";
+    int height = 0, i = 1, j = 1;
+
+    cout << "Enter height of triangle: ";
+    getline(std::cin, num);
+    stringstream(num) >> height;
+
+    do{
+        for(j = i; j < height; j++){
+            cout << "  ";
+        }
+        for(j = 1; j <= (2*i-1); j++){
+            cout << "* ";
+        }
+        cout << endl;
+        i++;
+    }while(i<=height);
+    
+    
+    // ********************** DO NOT CHANGE **********************
+    // Print a new line and ask user for any key before exiting
+    // ***********************************************************
+    cout << "" << endl;
+    _pause();
+    return EXIT_SUCCESS;
+}
